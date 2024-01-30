e973afdd6033490e6c03a4b6524aa1b1187cd0a2
Aakash Rao <mrao.aakash@gmail.com>
2024-01-30 13:07:15 +0530

    Added paths in runfile

--- runfiles/run_norm.sh
+++ runfiles/run_norm.sh
@@ -11,16 +11,12 @@ module load compiler/anaconda3
 eval "$(conda shell.bash hook)" 
 conda activate COL_NORM
 
-BASEPATH=/home/aakash.rao_asp24
-
-PATHFROMDIR=$BASEPATH/capstone-thesis/utils/normalization/pick_images.py
 
 echo "Running normalization script"
-echo $PATHFROMDIR
 
-python $PATHFRO/pick_images.py \
-    --input_dir $BASEPATH/capstone-thesis/data/NuclsEvalSet/rgb \
-    --output_dir $BASEPATH/capstone-thesis/data/norm_images/ \
+python /home/aakash.rao_asp24/capstone-thesis/utils/normalization/pick_images.py \
+    --input_dir /home/aakash.rao_asp24/capstone-thesis/data/NuclsEvalSet/rgb \
+    --output_dir /home/aakash.rao_asp24/capstone-thesis/data/norm_images/ \
     --n 5 \
     --i 5 \
     --seed 42 \
